536fcd4f63dd7cbab113672fb6eebd1462b960cf
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-03-15 21:46:32 +0400

    feat: add header component

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,21 +1,26 @@
+import './App.css';
+import { Header } from './components/Header/Header.component';
 import React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
 export function WrappedApp() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
