de08c87f9a630fcbf349c7afa06f34acb45a012c
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-03-19 19:41:27 +0400

    fix: header title visited from other links

--- react-components/src/pages/About.tsx
+++ react-components/src/pages/About.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export function About() {
   return (
     <div>
       <h1>About us page</h1>
-      <Link to="/">GO HOME</Link>
     </div>
   );
 }
--- react-components/src/pages/NotFound.tsx
+++ react-components/src/pages/NotFound.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
     <div>
       <h1>Not Found</h1>
-      <Link to="/">GO HOME</Link>
     </div>
   );
 }
