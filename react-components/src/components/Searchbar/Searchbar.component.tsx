f2be9086ddedc85d2d022ce8bed06406b21af7c6
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-03-29 19:31:54 +0400

    refactor: searchbar as functional component

--- react-components/src/components/Searchbar/Searchbar.component.tsx
+++ react-components/src/components/Searchbar/Searchbar.component.tsx
@@ -1,49 +1,23 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './Searchbar.component.css';
 import { BsSearch } from 'react-icons/bs';
 
-interface Props {
-  children?: React.ReactNode;
-}
+export function SearchBar() {
+  const [query, setQuery] = useState(localStorage.getItem('inputValue') || '');
 
-interface State {
-  value: string;
-}
-
-export class SearchBar extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { value: localStorage.getItem('inputValue') || '' };
-  }
-
-  hadleChange(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ value: event.target.value });
+  const hadleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
     localStorage.setItem('inputValue', event.target.value);
-  }
+  };
 
-  componentDidMount(): void {
-    this.setState({ value: localStorage.getItem('inputValue') || '' });
-  }
-
-  componentWillUnmount(): void {
-    localStorage.setItem('inputValue', this.state.value);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <form className="search-bar">
-          <input
-            type="text"
-            value={this.state.value}
-            placeholder="search"
-            onInput={this.hadleChange.bind(this)}
-          ></input>
-          <button type="submit">
-            <BsSearch />
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <form className="search-bar">
+        <input type="text" value={query} placeholder="search" onInput={hadleChange}></input>
+        <button type="submit">
+          <BsSearch />
+        </button>
+      </form>
+    </div>
+  );
 }
