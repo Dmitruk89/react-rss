92f430e23c9e64de7ea83138bde72cd773b8ab61
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-04-05 17:45:30 +0400

    feat: add fetching characters by query

--- react-components/src/pages/Home.tsx
+++ react-components/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { SearchBar } from '../components/Searchbar/Searchbar.component';
+import { queryData, SearchBar } from '../components/Searchbar/Searchbar.component';
 import React, { useEffect, useState } from 'react';
 
 import { CardLIst } from '../components/CardList/CardList.component';
@@ -6,14 +6,27 @@ import { CardLIst } from '../components/CardList/CardList.component';
 export function Home() {
   const [characters, setCharachters] = useState(null);
 
-  useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character')
+  const getCards = (url: string) => {
+    fetch(url)
       .then((response) => response.json())
       .then((data) => setCharachters(data.results));
-  });
+  };
+
+  const handleSearchSubmit = (data: queryData | null) => {
+    if (data) {
+      getCards(`https://rickandmortyapi.com/api/character/?name=${data.name}`);
+    } else {
+      console.log('no such character!');
+    }
+  };
+
+  useEffect(() => {
+    getCards('https://rickandmortyapi.com/api/character');
+  }, []);
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSearchSubmit={handleSearchSubmit} />
       {characters && <CardLIst data={characters} />}
     </div>
   );
