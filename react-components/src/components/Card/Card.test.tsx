eaec14af9f53ea24dc7fe7399be627bd783e8c84
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-04-07 10:14:30 +0400

    feat: update test for card component

--- react-components/src/components/Card/Card.test.tsx
+++ react-components/src/components/Card/Card.test.tsx
@@ -1,30 +1,52 @@
-import { describe, it } from 'vitest';
+import { describe, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import React from 'react';
 import { Card } from './Card.component';
+import userEvent from '@testing-library/user-event';
 
-const Product = {
-  category: 'office',
-  description: 'Working chair dark-grey',
-  id: 1,
-  img: 'https://www.ikea.com/ru/ru/images/products/bleckberget-blekberget-rabochiy-stul-idekulla-temno-seryy__0814728_pe776013_s5.jpg?f=xl',
-  name: 'BLECKBERGET',
-  price: 59,
+const Character = {
+  id: 2,
+  name: 'Morty Smith',
+  status: 'Alive',
+  species: 'Human',
+  type: '',
+  gender: 'Male',
+  origin: {
+    name: 'Earth',
+    url: 'https://rickandmortyapi.com/api/location/1',
+  },
+  location: {
+    name: 'Earth',
+    url: 'https://rickandmortyapi.com/api/location/20',
+  },
+  image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+  episode: [
+    'https://rickandmortyapi.com/api/episode/1',
+    'https://rickandmortyapi.com/api/episode/2',
+    // ...
+  ],
+  url: 'https://rickandmortyapi.com/api/character/2',
+  created: '2017-11-04T18:50:21.651Z',
 };
 
-describe('Card', () => {
-  it('renders correctly', () => {
-    render(<Card key={1} content={Product} />);
+const handleCardClick = vi.fn();
 
-    const productName = screen.getByText(/bleckberget/i);
-    const productDescription = screen.getByText(/Working chair dark-grey/i);
-    const productPrice = screen.getByText(/59\.00 €/i);
+describe('Card', () => {
+  const user = userEvent.setup();
+  it('renders correctly', () => {
+    render(<Card key={1} content={Character} onCardClick={handleCardClick} />);
+
+    const productName = screen.getByText(/Morty Smith/i);
     const displayedImage = document.querySelector('img') as HTMLImageElement;
 
     expect(productName).toBeInTheDocument();
-    expect(productDescription).toBeInTheDocument();
-    expect(productPrice).toBeInTheDocument();
-    expect(displayedImage.src).toContain('blekberget');
+    expect(displayedImage.src).toContain('avatar/2.jpeg');
+  });
+  it('calls handleCardClick on card click', async () => {
+    render(<Card key={1} content={Character} onCardClick={handleCardClick} />);
+    const productName = screen.getByText(/Morty Smith/i);
+    await user.click(productName);
+    expect(handleCardClick).toBeCalledTimes(1);
   });
 });
