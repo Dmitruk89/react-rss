f8bdae4d3272ca591bdb0ecf3358333e85903585
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-04-12 12:46:04 +0400

    fix: tests for search component

--- react-components/src/components/Searchbar/Searchbar.test.tsx
+++ react-components/src/components/Searchbar/Searchbar.test.tsx
@@ -1,5 +1,6 @@
 import { describe, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { renderWithProviders } from '../../utils/redux-render';
 
 import React from 'react';
 import { SearchBar } from './Searchbar.component';
@@ -10,49 +11,20 @@ import configureStore from 'redux-mock-store';
 const handleSearchSubmit = vi.fn();
 
 describe('Searchbar', () => {
-  const initialState = {
-    search: {
-      value: '',
-    },
-  };
-  const mockStore = configureStore();
-  const store = mockStore(initialState);
   const user = userEvent.setup();
   it('renders correctly', () => {
-    render(
-      <Provider store={store}>
-        <SearchBar onSearchSubmit={handleSearchSubmit} />
-      </Provider>
-    );
+    renderWithProviders(<SearchBar onSearchSubmit={handleSearchSubmit} />);
     const input = screen.getByRole('textbox');
     expect(input).toBeInTheDocument();
   });
   it('handles user input', async () => {
-    render(
-      <Provider store={store}>
-        <SearchBar onSearchSubmit={handleSearchSubmit} />
-      </Provider>
-    );
+    renderWithProviders(<SearchBar onSearchSubmit={handleSearchSubmit} />);
     const input = screen.getByRole('textbox');
     await user.type(input, 'hello');
     expect(input).toHaveValue('hello');
   });
-  it('saves user input', async () => {
-    render(
-      <Provider store={store}>
-        <SearchBar onSearchSubmit={handleSearchSubmit} />
-      </Provider>
-    );
-    const input = screen.getByRole('textbox');
-    window.location.reload();
-    expect(input).toHaveValue('hello');
-  });
   it('calls handleSearchSubmit on close-button click', async () => {
-    render(
-      <Provider store={store}>
-        <SearchBar onSearchSubmit={handleSearchSubmit} />
-      </Provider>
-    );
+    renderWithProviders(<SearchBar onSearchSubmit={handleSearchSubmit} />);
     const searchBtn = screen.getByTestId('search-button-element');
     await user.click(searchBtn);
     expect(handleSearchSubmit).toBeCalledTimes(1);
