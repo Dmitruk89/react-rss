2bd3dd92491618240aa9709542be4f1e145f6ec4
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-04-23 11:33:54 +0400

    feat: add e2e coverage for navigation

--- react-components/cypress/e2e/navigattion/navigation.cy.ts
+++ react-components/cypress/e2e/navigattion/navigation.cy.ts
@@ -9,14 +9,26 @@ describe('Home page interaction', () => {
     cy.get('.active').contains('Home');
   });
   it('should visit About page', () => {
+    cy.visit('/about');
+    cy.get('.active').contains('About us');
+    cy.get('h1').contains('About us page');
+  });
+  it('should visit About page on nav-link clik', () => {
     cy.visit('/');
     cy.get('a').contains('About us').click();
     cy.get('.active').contains('About us');
+    cy.get('h1').contains('About us page');
   });
   it('should visit Form page', () => {
+    cy.visit('/form');
+    cy.get('.active').contains('Form');
+    cy.get('h1').contains('Form page');
+  });
+  it('should visit Form page on nav-link clik', () => {
     cy.visit('/');
     cy.get('a').contains('Form').click();
     cy.get('.active').contains('Form');
+    cy.get('h1').contains('Form page');
   });
   it('should show not found page', () => {
     cy.visit('/not-existing');
