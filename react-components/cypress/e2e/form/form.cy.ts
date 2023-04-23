2ef33f8b2780898c341283ec359e498c6f40c385
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-04-23 11:19:39 +0400

    feat: add e2e for form page

---
+++ react-components/cypress/e2e/form/form.cy.ts
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+describe('Form interaction', () => {
+  afterEach(() => {
+    cy.window().trigger('unload');
+  });
+  it('should not submit with invalid inputs', () => {
+    cy.visit('/form');
+    cy.get('.form input[type="submit"]').click();
+    cy.get('.notification-message').should('have.text', 'Form was not submitted');
+    cy.get('.validation').should('not.have.text', '');
+    cy.wait(3000);
+    cy.get('.notification-message').should('not.exist');
+  });
+  it('should submit with valid inputs', () => {
+    cy.visit('/form');
+    cy.get('input[name="name"]').type('John');
+    cy.get('input[name="agree"]').click();
+    cy.get('input[type="file"]').selectFile('cypress/fixtures/rslogo.jpg');
+    cy.get('.form input[type="submit"]').click();
+    cy.get('.notification-message').should('have.text', 'Form was successfully submitted');
+    cy.get('.validation').should('have.text', '');
+    cy.wait(3000);
+    cy.get('.notification-message').should('not.exist');
+  });
+});
